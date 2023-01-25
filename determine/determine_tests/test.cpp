63d71b04e7ea2430a9a854e41016cdd8ddfd8860
AndreyM <222mushkin@gmail.com>
2023-01-25 22:04:44 +0300

    moved catch header file to solution root directory, added automated tests for determine program

---
+++ determine/determine_tests/test.cpp
@@ -0,0 +1,34 @@
+#include "catch.hpp"
+#include <sstream>
+
+#include "fsm/io.h"
+#include "MachineDeterminer.h"
+
+void Proccess(std::istream& input, std::ostream& output)
+{
+	auto machine = fsm::ReadCsv(input);
+	fsm::WriteCsv(output, MachineDeterminer::Determine(machine));
+}
+
+TEST_CASE("with 'e' signal and multi transitions")
+{
+	Logger::Quiet(true);
+
+	std::istringstream in(R"(;;;;;F;;;;
+;S1;S2;S3;S4;S5;S6;S7;S8;S9
+a;;S3;;;;S7;;S9;
+b;;;S4;;;;S5;;
+c;;;;S5;;;;;S5
+e;S2,S6,S8;;;;S1;;;;
+)");
+
+	std::ostringstream out;
+	Proccess(in, out);
+
+	REQUIRE(out.str() == R"(;;;F;F
+;S0;S1;S2;S3
+a;S1;-;S1;S1
+b;-;S2;-;-
+c;-;S3;S3;-
+)");
+}
